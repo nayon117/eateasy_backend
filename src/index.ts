494cad9c2bb68778bd02d50b668bf7a778130219
nayon117 <hasibul.nayon1@gmail.com>
2024-09-17 07:47:29 +0600

    connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+// connect to database
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database"));
+
+// middleware  
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
-app.get("/test", async(req:Request, res:Response)=>{
-    res.json({message:"Hello World"})
-})
-
-app.listen(5000, () =>{
-    console.log("Server running on port 5000")
-})
+// routes and listener
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
