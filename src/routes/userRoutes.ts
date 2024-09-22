0c1e8989fd9669a9ee308d8e75122a77f90cbc05
nayon117 <hasibul.nayon1@gmail.com>
2024-09-22 06:46:31 +0600

    validate token

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import userController from '../controllers/userController'
+import { jwtCheck } from '../middleware/auth'
 
 const router = express.Router()
 
-router.post('/' , userController.createCurrentUser)
+router.post('/',jwtCheck, userController.createCurrentUser)
 
 
 export default router
